e94595b2eb0b213b16041960a72b4aeaa0aacbc9
Leo Kalshteyn <leo.kalshteyn@gmail.com>
2019-08-02 23:15:06 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
-
-end
+ActiveRecord::Schema.define(version: 20160119215120) do
+  create_table "classroom_students", force: :cascade do |t|
+    t.integer  "classroom_id"
+    t.integer  "student_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
+  create_table "classrooms", force: :cascade do |t|
+    t.string   "course_name"
