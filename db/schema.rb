eb43508c130c4d4ee2c41e7493cae5068fd39417
Leo Kalshteyn <leo.kalshteyn@gmail.com>
2019-08-02 23:15:14 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -19,3 +19,15 @@ ActiveRecord::Schema.define(version: 20160119215120) do
   end
   create_table "classrooms", force: :cascade do |t|
     t.string   "course_name"
+    t.string   "semester"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+  create_table "students", force: :cascade do |t|
+    t.string   "name"
+    t.string   "hometown"
+    t.datetime "birthday"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+end
